20f83a5a658605547278bc0d13df2654e2d1c54e
Viacheslav Olshevskyi <viacheslavolshevskyi@gmail.com>
2017-11-03 08:51:47 +0200

    Version 1

---
+++ HomeWork1/HomeWork1/Devices.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork1
+{
+    public class Devices
+    {
+        public string name;
+        public void Print()
+        {
+            Console.WriteLine("Device's name" + name);
+        }
+        public interface IChange
+        {
+            void Chng(string name);
+        }
+        public class Phone : IChange
+        {
+            public int year;
+            public string country;
+            public void Chng(string name)
+            {
+                Console.WriteLine("Word" + name + "has" + name.Length + "letters");
+            }
+        }
+        public interface IShowYear
+        {
+            void ShowYear(int year);
+        }
+        public class Notebook : IShowYear
+        {
+            public int country;
+            public int year;
+            public void ShowYear(int year)
+            {
+                Console.WriteLine("Notebook was made in " + year);
+            }
+        }
+    }
+}
---
+++ HomeWork1/HomeWork1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Devices [] cellPhone = new Devices[3];
+            
+            for(int i=0; i < cellPhone.Length; i++)
+            {
+                cellPhone[i] = new Devices();
+            }
+            cellPhone[0].name = "Nokia";
+            cellPhone[1].name = "Lenovo";
+            cellPhone[2].name = "Samsung";
+            for(int i=0; i < cellPhone.Length; i++)
+            {
+                Console.WriteLine(i+1 + ". " + cellPhone[i].name);
+            }
+            //Devices.Phone Nokia = new Devices.Phone
+            //{
+            //    year = 1995,
+            //    country = "Finland"
+            //
+            //};
+            //Devices.IShowYear year = new Devices.IShowYear
+            //{
+
+//            }
+           
+
+            Console.ReadKey();
+        }
+    }
+}
